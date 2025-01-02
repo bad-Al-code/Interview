2fc32c149d460298611d60bbe7cefb7ad3d73f83
badAl <badalsingh120403@gmail.com>
2025-01-02 15:52:49 +0530

    theory: window sliding technique
    - describe when to use thie technique, and also when avoid this techique
    {mostly if given data structure does nnot allow rnadom access of
    element}

---
+++ DSA/LeetCode/EASY/Array/ArrayTechniquesTheory.ts
@@ -0,0 +1,9 @@
+/**
+ * When use window sliding pattern?
+ * - repeated computations on a contiguous set of data elements.
+ * - when window moves, it takes O(1) or slower growing function for computations.
+ *
+ * When do not use window sliding pattern?
+ * - input data structure does not support random access.
+ * - process the entire array without segmentation.
+ */
