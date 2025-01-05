2c9fc770ce74cb90b85d718924f8d5f043ad367b
badAl <badalsingh120403@gmail.com>
2025-01-06 00:09:54 +0530

    IDK: i still dont know how it works, copied from chatgpt. Forgive me

--- DSA/LeetCode/EASY/Recursion/PascalTriangle.ts
+++ DSA/LeetCode/EASY/Recursion/PascalTriangle.ts
@@ -1,3 +1,15 @@
 function printPascal(a: number): number[] {
-    return [];
+    let line = [1];
+
+    if (a === 0) return [1];
+    else {
+        let previousLine = printPascal(a - 1);
+
+        for (let i = 0; i < previousLine.length - 1; i++) {
+            line.push(previousLine[i] + previousLine[i + 1]);
+        }
+
+        line.push(1);
+    }
+    return line;
 }
