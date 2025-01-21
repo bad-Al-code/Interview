31c6233d8e66736f3d926713011b28da2027f5dc
badAl <badalsingh120403@gmail.com>
2025-01-21 21:30:41 +0530

    fix: use memoization to fix TLE

--- DSA/LeetCode/EASY/Graph/UniquePath.ts
+++ DSA/LeetCode/EASY/Graph/UniquePath.ts
@@ -1,4 +1,17 @@
-function dfs(r: number, c: number, ROWS: number, COLS: number): number {
+/**
+ * @param r
+ * @param c
+ * @param ROWS
+ * @param COLS
+ * @returns
+ */
+function dfs(
+    r: number,
+    c: number,
+    ROWS: number,
+    COLS: number,
+    memo: number[][],
+): number {
     if (r >= ROWS || c >= COLS) {
         return 0;
     }
@@ -7,12 +20,25 @@ function dfs(r: number, c: number, ROWS: number, COLS: number): number {
         return 1;
     }
 
-    const down = dfs(r + 1, c, ROWS, COLS);
-    const right = dfs(r, c + 1, ROWS, COLS);
+    if (memo[r][c] !== -1) {
+        return memo[r][c];
+    }
 
-    return down + right;
+    const down = dfs(r + 1, c, ROWS, COLS, memo);
+    const right = dfs(r, c + 1, ROWS, COLS, memo);
+
+    memo[r][c] = down + right;
+
+    return memo[r][c];
 }
 
+/**
+ * @param m
+ * @param n
+ * @returns
+ */
 function uniquePaths(m: number, n: number): number {
-    return dfs(0, 0, m, n);
+    const memo: number[][] = Array.from({ length: m }, () => Array(n).fill(-1));
+
+    return dfs(0, 0, m, n, memo);
 }
