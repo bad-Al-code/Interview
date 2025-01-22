40d3dd4a39d9e4eb54c26a7c1c3c8b8b5b2600d4
badAl <badalsingh120403@gmail.com>
2025-01-23 02:11:41 +0530

    fix: use dfs to solve maximum area of islands

--- DSA/LeetCode/EASY/Graph/MaxAreaOfIsland.ts
+++ DSA/LeetCode/EASY/Graph/MaxAreaOfIsland.ts
@@ -1,46 +1,52 @@
-/**
- * @param grid
- * @param r
- * @param c
- * @returns
- */
-function dfs(grid: number[][], r: number, c: number): number {
-    const ROWS = grid.length;
-    const COLS = grid[0].length;
-
-    if (r < 0 || r >= ROWS || c < 0 || c >= COLS || grid[r][c] === 0) {
-        return 0;
-    }
-
-    grid[r][c] = 0;
-
-    let area = 1;
-
-    area += dfs(grid, r + 1, c);
-    area += dfs(grid, r - 1, c);
-    area += dfs(grid, r, c + 1);
-    area += dfs(grid, r, c - 1);
-
-    return area;
-}
-
-/**
- * @param grid
- * @returns
- */
 function maxAreaOfIsland(grid: number[][]): number {
     const ROWS = grid.length;
     const COLS = grid[0].length;
-    let maxArea = 0;
 
-    for (let r = 0; r < ROWS; r++) {
-        for (let c = 0; c < COLS; c++) {
-            if (grid[r][c] === 1) {
-                const area = dfs(grid, r, c);
-                maxArea = Math.max(maxArea, area);
+    if (ROWS === 0 || COLS === 0) {
+        return 0;
+    }
+
+    const visit: Set<string> = new Set();
+    let count = 0;
+
+    const dfs = (r: number, c: number): number => {
+        if (
+            r < 0 ||
+            c < 0 ||
+            r >= ROWS ||
+            c >= COLS ||
+            grid[r][c] === 0 ||
+            visit.has(`${r},${c}`)
+        ) {
+            return 0;
+        }
+
+        visit.add(`${r},${c}`);
+
+        return (
+            1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+        );
+    };
+
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
+            if (grid[i][j] === 1 && !visit.has(`${i},${j}`)) {
+                const area = dfs(i, j);
+
+                count = Math.max(area, count);
             }
         }
     }
 
-    return maxArea;
+    return count;
 }
+
+const grid = [
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 1],
+];
+
+console.log(maxAreaOfIsland(grid));
