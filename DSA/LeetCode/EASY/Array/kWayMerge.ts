cac7073b09b6aafea649dd6173454ba7def0a2b6
Badal <badalsingh120403@gmail.com>
2025-02-18 20:03:58 +0530

    theory: k-way merge

---
+++ DSA/LeetCode/EASY/Array/kWayMerge.ts
@@ -0,0 +1,9 @@
+/**
+ * merge k sorted data structures, such as arrays and linked list, into a singe data structure that maintainst the
+ * sorterd order.
+ *
+ * IDEA:
+ * 	- repeateedly select the smallest (or largest dependeing on the sorting order) element among the k input lst and add
+ * 	it to the output list.
+ * 	- continue untill all elemnts from all input lists ave been added to the output list
+ */
