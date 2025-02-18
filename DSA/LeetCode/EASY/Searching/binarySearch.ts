05573a35cd9b5f13f3e08c01e97b2d53cc83f04a
Badal <badalsingh120403@gmail.com>
2025-02-18 20:10:12 +0530

    feat(searching): implement basic binary search

---
+++ DSA/LeetCode/EASY/Searching/binarySearch.ts
@@ -0,0 +1,15 @@
+function binarySearch(nums: number[], target: number): number {
+    let low = 0;
+    let high = nums.length - 1;
+    while (low <= high) {
+        let mid = low + Math.floor((high - low) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (target < nums[mid]) {
+            high = mid - 1;
+        } else if (target > nums[mid]) {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
