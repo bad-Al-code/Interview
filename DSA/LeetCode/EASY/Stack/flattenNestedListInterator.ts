4fa595c90c76a27b38c4c58f191071e8cd60ab2c
Badal <badalsingh120403@gmail.com>
2025-02-24 04:31:27 +0530

    chore(stack): implement NestedInteger class

--- DSA/LeetCode/EASY/Stack/flattenNestedListInterator.ts
+++ DSA/LeetCode/EASY/Stack/flattenNestedListInterator.ts
@@ -1,15 +1,49 @@
-class NestedInteger {
-    constructor(value?: number) {}
+interface INestedInteger {
+    isInteger(): boolean;
+    getInteger(): number | null;
+    setInteger(value: number): void;
+    add(ni: NestedInteger): void;
+    getList(): NestedInteger[] | null;
+}
 
-    isInteger(): boolean {}
+class NestedInteger implements INestedInteger {
+    private integer: number | null;
+    private nList: NestedInteger[] | null;
 
-    getInteger(): number | null {}
+    constructor(integer: number | null = null) {
+        if (integer !== null) {
+            this.integer = integer;
+            this.nList = null;
+        } else {
+            this.integer = null;
+            this.nList = [];
+        }
+    }
 
-    setInteger(value: number) {}
+    isInteger(): boolean {
+        return this.integer !== null;
+    }
 
-    add(elem: NestedInteger) {}
+    getInteger(): number | null {
+        return this.integer;
+    }
 
-    getList(): NestedInteger[] {}
+    setInteger(value: number): void {
+        this.nList = null;
+        this.integer = value;
+    }
+
+    add(ni: NestedInteger): void {
+        if (this.integer !== null) {
+            this.nList = [new NestedInteger(this.integer)];
+            this.integer = null;
+        }
+        this.nList!.push(ni);
+    }
+
+    getList(): NestedInteger[] | null {
+        return this.nList;
+    }
 }
 
 class NestedIterator {
@@ -24,9 +58,9 @@ class NestedIterator {
             if (top.isInteger()) return true;
 
             let topList = this.stack.pop()!.getList();
-            let i = topList?.length - 1;
+            let i = topList!.length - 1;
             while (i >= 0) {
-                this.stack.push(topList[i]);
+                this.stack.push(topList![i]);
                 i -= 1;
             }
         }
@@ -49,3 +83,89 @@ class NestedIterator {
  * var a: number[] = []
  * while (obj.hasNext()) a.push(obj.next());
  */
+
+function main() {
+    let inputs = [
+            '[1, [2, 3], 4]',
+            '[3, [2, 3, 4], 4, [2, 3]]',
+            '[[2, 3], 3, [2, 3], 4, [2, 3, 4, 5]]',
+            '[1, [3, [4, [5, 6], 7], 8], 9]',
+            '[[2, 3, [2, 3]]]',
+        ],
+        itr = [];
+
+    // Test Case #1
+    let nestedList = [];
+    let l1 = new NestedInteger();
+    nestedList.push(new NestedInteger(1));
+    l1.add(new NestedInteger(2));
+    l1.add(new NestedInteger(3));
+    nestedList.push(l1);
+    nestedList.push(new NestedInteger(4));
+    itr.push(new NestedIterator(nestedList));
+
+    // Test Case #2
+    nestedList = [];
+    let l2 = new NestedInteger();
+    nestedList.push(new NestedInteger(3));
+    l2.add(new NestedInteger(2));
+    l2.add(new NestedInteger(3));
+    l2.add(new NestedInteger(4));
+    nestedList.push(l2);
+    nestedList.push(new NestedInteger(4));
+    nestedList.push(l1);
+    itr.push(new NestedIterator(nestedList));
+
+    // Test Case #3
+    nestedList = [];
+    let l3 = new NestedInteger();
+    nestedList.push(l1);
+    nestedList.push(new NestedInteger(3));
+    l3.add(new NestedInteger(2));
+    l3.add(new NestedInteger(3));
+    l3.add(new NestedInteger(4));
+    l3.add(new NestedInteger(5));
+    nestedList.push(l1);
+    nestedList.push(new NestedInteger(4));
+    nestedList.push(l3);
+    itr.push(new NestedIterator(nestedList));
+
+    // Test case #4
+    nestedList = [];
+    nestedList.push(new NestedInteger(1));
+    l1 = new NestedInteger();
+    l1.add(new NestedInteger(5));
+    l1.add(new NestedInteger(6));
+    l2 = new NestedInteger();
+    l2.add(new NestedInteger(4));
+    l2.add(l1);
+    l2.add(new NestedInteger(7));
+    l3 = new NestedInteger();
+    l3.add(new NestedInteger(3));
+    l3.add(l2);
+    l3.add(new NestedInteger(8));
+    nestedList.push(l3);
+    nestedList.push(new NestedInteger(9));
+    itr.push(new NestedIterator(nestedList));
+
+    // TEST CASE 5: [[2, 3, [2, 3]]]
+    nestedList = [];
+    l2 = new NestedInteger();
+    l2.add(new NestedInteger(2));
+    l2.add(new NestedInteger(3));
+    l3 = new NestedInteger();
+    l3.add(new NestedInteger(2));
+    l3.add(new NestedInteger(3));
+    l3.add(l2);
+    nestedList.push(l3);
+    itr.push(new NestedIterator(nestedList));
+
+    for (let i = 0; i < itr.length; i++) {
+        console.log(i + 1 + '.\tOriginal structure: ', inputs[i]);
+        console.log('\n\tOutput:\n');
+        while (itr[i].hasNext()) console.log('\titr.next(): ', itr[i].next());
+        console.log('-'.repeat(100));
+    }
+}
+
+main();
