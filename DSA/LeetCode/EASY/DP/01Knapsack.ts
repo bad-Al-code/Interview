7b173531be458ba72aad9432eaa6bf4e3e471dd4
Badal <badalsingh120403@gmail.com>
2025-03-01 15:38:14 +0530

    tabulation

--- DSA/LeetCode/EASY/DP/01Knapsack.ts
+++ DSA/LeetCode/EASY/DP/01Knapsack.ts
@@ -1,4 +1,4 @@
-export function helper(
+function helper(
     capcaity: number,
     weights: number[],
     values: number[],
@@ -34,11 +34,29 @@ function maxKnapsack(
     values: number[],
 ): number {
     const n = weights.length;
-    const dp: number[][] = Array.from({ length: n + 1 }, () =>
-        Array(capcaity + 1).fill(-1),
-    );
+    // const dp: number[][] = Array.from({ length: n + 1 }, () =>
+    //     Array(capcaity + 1).fill(-1),
+    // );
+    //
+    // return helper(capcaity, weights, values, n, dp);
 
-    return helper(capcaity, weights, values, n, dp);
+    const dp: number[][] = new Array(n + 1)
+        .fill(0)
+        .map(() => new Array(capcaity + 1).fill(0));
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= capcaity; j++) {
+            if (weights[i - 1] <= j) {
+                dp[i][j] = Math.max(
+                    values[i - 1] + dp[i - 1][j - weights[i - 1]],
+                    dp[i - 1][j],
+                );
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[n][capcaity];
 }
 
 function main() {
