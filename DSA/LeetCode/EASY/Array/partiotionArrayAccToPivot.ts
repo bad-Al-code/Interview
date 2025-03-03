de4c112b638d8adc18ed6fca1a294f9d8ebd7ab8
Badal <badalsingh120403@gmail.com>
2025-03-03 11:47:16 +0530

    chore: solve pivot element partition efficiently without any space other than result arrray using two pointers

--- DSA/LeetCode/EASY/Array/partiotionArrayAccToPivot.ts
+++ DSA/LeetCode/EASY/Array/partiotionArrayAccToPivot.ts
@@ -1,3 +1,34 @@
+function pivotElementEfficient(nums: number[], pivot: number): number[] {
+    const n = nums.length;
+    const result = new Array(n).fill(0);
+
+    let i = 0;
+    let j = n - 1;
+    let i1 = 0;
+    let j1 = n - 1;
+
+    while (i < n) {
+        if (nums[i] < pivot) {
+            result[i1] = nums[i];
+            i1++;
+        }
+        if (nums[j] > pivot) {
+            result[j1] = nums[j];
+            j1--;
+        }
+        i++;
+        j--;
+    }
+
+    while (i1 <= j1) {
+        result[i1] = result[j1] = pivot;
+        i1++;
+        j1--;
+    }
+
+    return result;
+}
+
 function pivotArray(nums: number[], pivot: number): number[] {
     const smallerElements: number[] = [];
     const pivotElements: number[] = [];
@@ -26,7 +57,9 @@ function pivotArray(nums: number[], pivot: number): number[] {
     }
     console.log('-'.repeat(100));
 
-    const result = smallerElements.concat(pivotElements.concat(largerElements));
+    const result: number[] = smallerElements.concat(
+        pivotElements.concat(largerElements),
+    );
 
     return result;
 }
@@ -35,3 +68,4 @@ export const nums = [9, 12, 5, 10, 14, 3, 10];
 const pivot = 10;
 
 console.log(pivotArray(nums, pivot));
+console.log(pivotElementEfficient(nums, pivot));
