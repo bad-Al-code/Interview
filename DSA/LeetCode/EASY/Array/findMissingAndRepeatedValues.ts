90526d0d6f0d5a2720ba120067377bad52840f3c
Badal <badalsingh120403@gmail.com>
2025-03-06 22:49:04 +0530

    fix: find missing vlue and update missing variable correctly

--- DSA/LeetCode/EASY/Array/findMissingAndRepeatedValues.ts
+++ DSA/LeetCode/EASY/Array/findMissingAndRepeatedValues.ts
@@ -15,7 +15,7 @@ function findMissingAndRepeatedValues(grid: number[][]): number[] {
         }
     }
 
-    for (let i = 1; i <= gridLen; i++) {
+    for (let i = 1; i <= gridLen * gridLen; i++) {
         if (counts[i] === 2) {
             repeated = i;
         } else if (counts[i] === 0) {
@@ -25,3 +25,20 @@ function findMissingAndRepeatedValues(grid: number[][]): number[] {
 
     return [repeated, missing];
 }
+
+export const grid1 = [
+    [1, 3],
+    [2, 2],
+];
+export const grid2 = [
+    [9, 1, 7],
+    [8, 9, 2],
+    [3, 4, 6],
+];
+
+console.log(
+    `Missing and repeated values for grid1: ${findMissingAndRepeatedValues(grid1)}`,
+);
+console.log(
+    `Missing and repeated values for grid2: ${findMissingAndRepeatedValues(grid2)}`,
+);
