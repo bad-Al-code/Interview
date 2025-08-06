87c0056d9b6bc2dcdf3c5ea4f9902b1d02cda987
Badal <badalsingh120403@gmail.com>
2025-08-06 22:06:49 +0000

    array

---
+++ educative/data-structures/array.js
@@ -0,0 +1,184 @@
+function removeElement(arr) {
+  const result = [];
+
+  for (let element of arr) {
+    if (element % 2 !== 0) {
+      result.push(element);
+    }
+  }
+
+  return result
+}
+
+function removeElementUsingFilter(arr) {
+  return arr.filter(element => element % 2 !== 0);
+}
+
+console.log(removeElement([1, 2, 3, 4, 5, 6]))
+console.log(removeElementUsingFilter([1, 2, 3, 4, 5, 6]))
+
+// Merge two Sort
+function mergeArrays(arr1, arr2) {
+  // return arr1.concat(arr2).sort();
+  return [...arr1, ...arr2].sort((a, b) => a - b)
+}
+
+function mergeArraysOptimize(arr1, arr2) {
+  let result = [];
+
+  let i = j = 0;
+  while ((i < arr1.length) && (j < arr2.length)) {
+    if (arr1[i] < arr2[j]) { result.push(arr1[i]); i++ }
+    else { result.push(arr2[j]); j++ }
+  }
+
+  if (i <= arr1.length - 1) {
+    arr1.splice(0, i);
+    result = result.concat(arr1);
+  }
+  else if (i <= arr2.length - 1) {
+    arr2.splice(0, j)
+    result = result.concat(arr2)
+  }
+
+  return result;
+}
+
+console.log(mergeArrays([1, 2, 3], [6, 5, 4]))
+console.log(mergeArrays([4, 5, 6], [-2, -1, 0, 7]))
+console.log(mergeArraysOptimize([4, 5, 6], [-2, -1, 0, 7]))
+
+// Find Sum 
+function findSum(arr, value) {
+
+  arr.sort(function (a, b) {
+    return a - b
+  })
+
+  var index1 = 0,
+    index2 = arr.length - 1,
+    result = [],
+    sum = 0;
+
+  while (index1 != index2) {
+    sum = arr[index1] + arr[index2]
+
+    if (sum < value) {
+      index1++;
+    } else if (sum > value) {
+      index2--;
+    } else {
+      result.push(arr[index1]);
+      result.push(arr[index2]);
+      return result;
+
+    }
+  }
+  return result;
+}
+
+
+console.log(findSum([1, 2, 3, 4], 5))
+console.log(findSum([1, 2, 3, 4], 10))
+
+// Find Product 
+function findProduct(arr) {
+
+  return arr;
+}
+
+import { getCategories } from "@/app/(admin)/actions";
+import { getCurrentUser } from "@/app/(auth)/actions";
+import { getCourseDetails } from "@/app/courses/actions";
+import { IconBadge } from "@/components/shared/icon-badge";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { File, LayoutDashboard, ListChecks } from "lucide-react";
+import { redirect } from "next/navigation";
+
+export default async function CourseEditorPage({
+  params,
+}: {
+  params: { courseId: string };
+}) {
+  const { courseId } = params;
+
+  const user = await getCurrentUser();
+  const course = await getCourseDetails(courseId);
+  const categories = await getCategories();
+
+  if (!course || !user || user.userId !== course.instructorId) {
+    redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+    course.modules.some((module: any) =>
+      module.lessons.some((lesson: any) => lesson.isPublished)
+    ),
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-2">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+
+      <Tabs defaultValue="details" className="w-full mt-8">
+        <TabsList className="flex overflow-x-auto no-scrollbar ">
+          <TabsTrigger value="details">
+            <LayoutDashboard className="h-4 w-4 mr-0" />
+            Customize Details
+          </TabsTrigger>
+          <TabsTrigger value="curriculum">
+            <ListChecks className="h-4 w-4 mr-0" />
+            Curriculum
+          </TabsTrigger>
+          <TabsTrigger value="attachments">
+            <File className="h-4 w-4 mr-0" />
+            Attachments & Resources
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="details" className="mt-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={LayoutDashboard} />
+                <h2 className="text-xl">Course Details</h2>
+              </div>
+              <p className="mt-4">Details form will go here.</p>
+            </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={File} />
+                <h2 className="text-xl">Course Thumbnail</h2>
+              </div>
+              <p className="mt-4">Thumbnail uploader will go here.</p>
+            </div>
+          </div>
+        </TabsContent>
+        <TabsContent value="curriculum" className="mt-6">
+          <h2 className="text-xl font-medium">Course Curriculum</h2>
+          <p className="mt-4">Curriculum editor will go here.</p>
+        </TabsContent>
+        <TabsContent value="attachments" className="mt-6">
+          <h2 className="text-xl font-medium">Attachments & Resources</h2>
+          <p className="mt-4">Attachments manager will go here.</p>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
