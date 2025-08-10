b4bd4ae0abe1bfdda02703178c49bff9917041e4
Badal <badalsingh120403@gmail.com>
2025-08-10 23:30:44 +0000

    find first unique character

--- educative/data-structures/array.js
+++ educative/data-structures/array.js
@@ -118,3 +118,20 @@ function findMinimum(arr) {
 }
 
 console.log(findMinimum([9, 2, 3, 6]))
+
+function firstUniqChar(s: string): number {
+  const freq: Map<string, number> = new Map();
+
+  for (let char of s) {
+    freq.set(char, (freq.get(char) || 0) + 1)
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (freq.get(s[i]) === 1) {
+      return i
+    }
+  }
+  return -1
+};
+
+console.log(firstUnique([9, 2, 3, 2, 6, 6]))
